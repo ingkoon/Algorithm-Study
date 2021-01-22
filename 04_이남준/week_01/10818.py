8285a59e55d0f1dde3190130cedfb569f41189e6
Seokhwan Kwon 权锡焕 <63346802+Seokhwan-Kwon@users.noreply.github.com>
2021-01-22 23:42:23 +0900

    Delete 04_이남준/week_01 directory

--- 04_이남준/week_01/10818.py
+++
@@ -1,5 +0,0 @@
-num = input()
-
-number_list = list(map(int, input().split()))
-
-print(min(number_list), max(number_list))
--- 04_이남준/week_01/2439.py
+++
@@ -1,6 +0,0 @@
-number = int(input())
-
-for i in range(1, number + 1):
-    print(' ' * (number - i), end='')
-    print('*' * i)
-    i = i + 1
