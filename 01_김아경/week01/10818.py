78339cc8275b328728c2d403f449d48798191452
Seokhwan Kwon 权锡焕 <63346802+Seokhwan-Kwon@users.noreply.github.com>
2021-01-22 23:41:50 +0900

    Delete 01_김아경 directory

--- 01_김아경/week01/10818.py
+++
@@ -1,3 +0,0 @@
-N = int(input())
-num = [int(x) for x in input().split()]
-print(min(num), max(num))
--- 01_김아경/week01/2562.py
+++
@@ -1,2 +0,0 @@
-nums = [int(input()) for i in range(9)]
-print(max(nums), nums.index(max(nums))+1, sep='\n')
