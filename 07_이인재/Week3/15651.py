1551c727492ac8e5c71a3c703f7f2eceb9a3aa07
orlando <59018852+ingkoon@users.noreply.github.com>
2021-02-18 13:38:34 +0900

    Update 15651.py

--- 07_이인재/Week3/15651.py
+++ 07_이인재/Week3/15651.py
@@ -2,4 +2,21 @@ n, m = map(int, input().split(" "))
 
 arr = []
 
+def backtracking(index, n, m):
+    if index == m:
+        for i in result:
+            print(i,)
 
+N, M = map(int, input().split())
+out = []
+
+def solve(depth, N, M):
+    if depth == M:
+        print(' '.join(map(str, out)))
+        return
+    for i in range(N):
+        out.append(i+1)
+        solve(depth+1, N, M)
+        out.pop()
+
+solve(0, N, M)
